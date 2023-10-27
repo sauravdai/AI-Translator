1ffc5967317a3ae06073cd9fd3e4ccb464e8155e
sauravdai <sauravgartaula@gmail.com>
2023-10-27 16:53:33 +0545

    Separated the translate function to two different functions for input and translation

--- langchain1_withfuncs.py
+++ langchain1_withfuncs.py
@@ -5,21 +5,26 @@ from langchain.prompts.chat import ChatPromptTemplate
 os.environ["OPENAI_API_KEY"] = config('OPENAI_API_KEY')
 def main():
     while True:
-        print(translate())
+        sentence, target_language = get_user_input()
+        translation = translate_to_language(sentence, target_language)
+        print(translation)
 
 
-def translate():
+def get_user_input():
+    sentence = input("Enter the sentence that you want translated: ")
+    target_language = input("Which language do you wanna translate to? ")
+    return sentence, target_language
+
+
+def translate_to_language(sentence, output_language):
     chat = ChatOpenAI()
-    user_input = input("Enter the sentence that you want translated: ")
-    output_lang = input("Which language do you wanna translate to? ")
     template = "You are a helpful assistant that translates the user's input to {output_language}"
-    human_template = user_input
     chat_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", template),
-            ("human", human_template),
+            ("human", sentence),
         ])
-    messages = (chat_prompt.format_messages(output_language=output_lang))
+    messages = (chat_prompt.format_messages(output_language=output_language))
     return chat.predict_messages(messages)
 
 
