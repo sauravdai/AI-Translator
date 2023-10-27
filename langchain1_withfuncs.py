794245fa1a9518fa795b2e02fdf4139c93794c07
sauravdai <sauravgartaula@gmail.com>
2023-10-27 17:04:16 +0545

    Added a menu for different choices

--- langchain1_withfuncs.py
+++ langchain1_withfuncs.py
@@ -3,11 +3,27 @@ from decouple import config
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 os.environ["OPENAI_API_KEY"] = config('OPENAI_API_KEY')
+
 def main():
+    sentence, target_language = get_user_input()
     while True:
-        sentence, target_language = get_user_input()
         translation = translate_to_language(sentence, target_language)
         print(translation)
+
+        while True:  # This nested loop will keep asking for choice until a valid one is provided.
+            choice = input("Would you like to: \n1) Enter a new sentence\n2) Change translation language\n3) Exit\nChoose [1/2/3]: ").strip()
+
+            if choice == "1":
+                sentence, target_language = get_user_input()
+                break  # Break out of the choice loop to continue with main translation loop.
+            elif choice == "2":
+                target_language = input("Which language do you wanna translate to? ")
+                break  # Break out of the choice loop to continue with main translation loop.
+            elif choice == "3":
+                print("Goodbye!")
+                return  # Exit the main function completely.
+            else:
+                print("Invalid choice. Please select 1, 2, or 3.")
 
 
 def get_user_input():
